a626f3f185e43856a3febe42f178d315b00e1c08
Nodever2 <nddevern@mtu.edu>
2021-09-23 16:54:09 -0400

    Major update. Global player randomization; major code cleanup, organization, and documentation.
    Changed variable names to be more consistent:
    -Humans are human players
    -AIs are computer players
    -Both are considered Players
    -Variables are now named accordingly
    
    The game # each human player is in is now randomized (using the Fisher-yates shuffle, credit: https://bost.ocks.org/mike/shuffle/
    -Previously the first n humans were in game 1, then human (n+1) to human (2n) were in game 2, etc
    
    A lot of comments were added and cleaned up throughout the code for readability.
    -Some information that was already there is now presented in a more unified format.
    
    HWerror function made slightly more intuitive
    
    The results printing code is greatly improved. It used to manually print different strings with different amount of spaces for padding based on how high the player ID was. Now, it uses python's str.rjust() function to dynamically resize the strings based on the player count, evenly spacing the output for up to an infinite amount of players.

--- HWRNG.py
+++ HWRNG.py
@@ -4,15 +4,18 @@ Updated on Sep 22, 2021
 @author: Nodever2
 '''
 
-#THIS IS A GIT TEST CHANGE
-
-#NEW UPDATE! YOU CAN CHANGE THESE CONFIG VALUES TO MAKE THE ALGORITHM WORK DIFFERENTLY!
-#They all default at -1 meaning the program will automatically determine what they should be.
-ConfigMaxPlayersPerGame = 6#Default: 6. Will change to default if not set to an int between 1 and 6 (inclusive).
+'''##########################################
+############ Beginning of config ############
+##########################################'''
+ConfigMaxHumansPerGame = 6#Default: 6. Will change to default if not set to an int between 1 and 6 (inclusive).
 #todo: cleanup code (ADD FUCNTIONS, REMOVE REPEATED CODE), comments etc. maybe redo variable names? add isInstanceOf or whatever it is for config values.
 #and add optional alternate algorhthm for RNGing what team players are on that has an equal chance of all open slots
 #also make it so that the names of maps/leaders print with spaces instead of underscores if possible?
 #also randomize player numbers instead of the weird solution you have now
+
+'''##########################################
+############### End of config ###############
+##########################################'''
 
 import random
 import math
@@ -52,71 +55,90 @@ class HWMap(Enum):#Enum with all possible maps in Halo Wars
     FROZEN_VALLEY = 17
     GLACIAL_RAVINE = 18
 
-#function
-#input
-#input
-#returns
-def HWerror(errormsg, warning):
-    if (not(isinstance(warning, int))):
-        print("Error or warning encounterd. Invalid warning flag, should be 1 if warning, 0 if error. Terminating.")
-        warning = 0
+'''
+* HWError: function that displays error/warning message and terminates program if needed.
+* @param str errormsg: A string, the error message to be displayed.
+* @param int isError: 1 if error and program should be terminated, 0 if warning.
+'''
+def HWerror(errormsg, isError):
+    if (not(isinstance(isError, int)) or (isError < 0) or (isError > 1)):
+        print("Error or warning encountered. Invalid error flag, should be 1 if error, 0 if warning. Terminating.")
+        isError = 1
     if (isinstance(errormsg, str)):
         print(errormsg)
     else:
-        print("Error or warning encountered. Undefined error message.")
-    print()
-    if (warning == 0):
+        print("Error or warning encountered. Invalid error message.")
+    if (isError == 1):
+        print()
         input("Press enter to close...")
         exit()
 
 
 '''##########################################
-#############Beginning of script#############
+############ Beginning of script ############
 ##########################################'''
-if (not(isinstance(ConfigMaxPlayersPerGame,int))):
-    HWerror("Config Error: Max Players Per Game is not an integer. Terminating.", 0)
-    
+#STEP 1: INPUT    
 pcInput = input("How many players? ")
+
+#STEP 2: INITIALIZATION OF CORE VARIABLES
+#        AND ERROR CHECKING ON CONFIG AND INPUT
 try:
-    playerCount = int(pcInput)
+    humanCount = int(pcInput)
 except ValueError:
-    HWerror("Unexpected input. Terminating.", 0)
+    HWerror("Unexpected input. Terminating.", 1)
 
-if (playerCount <= 0):
-    HWerror("Unexpected input. Terminating.", 0)
+if (humanCount <= 0):
+    HWerror("Unexpected input. Terminating.", 1)
+
+if (not(isinstance(ConfigMaxHumansPerGame,int))):
+    HWerror("Config Error: Max Humans Per Game is not an integer. Terminating.", 1)
     
-remainingUnallocatedHumans = playerCount
-maxPlayers = 6
-if (ConfigMaxPlayersPerGame >= 1 and ConfigMaxPlayersPerGame <= 6):
-    maxPlayers = ConfigMaxPlayersPerGame
+maxHumans = 6
+if (ConfigMaxHumansPerGame >= 1 and ConfigMaxHumansPerGame <= 6):
+    maxHumans = ConfigMaxHumansPerGame
 else:
-    HWerror("Config Warning: Max Players Per Game is outside the allowed range. Using " + str(maxPlayers) + " instead.", 1)
-numGamesLeft = int(math.ceil(playerCount/maxPlayers))
+    HWerror("Config Warning: Max Humasns Per Game is outside the allowed range. Using " + str(maxHumans) + " instead.", 0)
 
-iteration = 0
-lastGamesHighestPlayerNum = 0
-while (remainingUnallocatedHumans > 0):                    #this code supports more than 6 players; it will divide the players evenly between 2 different games if so
+numGamesLeft = int(math.ceil(humanCount/maxHumans))#pre-calculated, later used to determine how many humans in each game
+remainingUnallocatedHumans = humanCount#keeps track of how many players there are left to allocate
+iteration = 0#this is used to display game #
+currentHumanIndex = 0#global index into randomizedHumanArray
+
+#STEP 3: RANDOMIZE THE PLAYER ORDER
+#This algorithm uses the Fisher-Yates shuffle. See https://bost.ocks.org/mike/shuffle/ for more info.
+randomizedHumanArray = [(i+1) for i in range(humanCount)]
+for i in range(humanCount):
+    randomEntry = random.randint(i,humanCount-1)
+    temp = randomizedHumanArray[i]
+    randomizedHumanArray[i] = randomizedHumanArray[randomEntry]
+    randomizedHumanArray[randomEntry] = temp
+    
+#STEP 4: ITERATE THROUGH EACH GAME
+while (remainingUnallocatedHumans > 0):
+    #STEP 4.1: SETUP AND CALCULATE HOW MANY HUMANS THIS GAME WILL HAVE
+    print()#print an empty line for aesthetics.
     iteration = iteration + 1                                            
     print("===========================")
     print("GAME " + str(iteration) + ": ")
     humansThisGame = -1
-    if (remainingUnallocatedHumans > maxPlayers):
+    if (remainingUnallocatedHumans > maxHumans):
         humansThisGame = int(math.ceil(remainingUnallocatedHumans/numGamesLeft))     #amount of players in each game (todoL figure this out before the loop I think)
     else:
         humansThisGame = remainingUnallocatedHumans
     remainingUnallocatedHumans -= humansThisGame
     
-    teamSize = -1;                                        #figure out team size (1v1, 2v2, or 3v3)
+    #STEP 4.2: RANDOMIZE TEAM SIZE (1v1, 2v2, or 3v3)
+    teamSize = -1
     if (humansThisGame <= 2):
         teamSize = random.randint(1,3)
     elif (humansThisGame <= 4):
         teamSize = random.randint(2,3)
     else:
         teamSize = 3
-    
     print("  This game will be a " + str(teamSize) + "v" + str(teamSize))
     
-    mapThisGame = HWMap(0)                                  #randomize map
+    #STEP 4.3: RANDOMIZE MAP BASED ON TEAM SIZE
+    mapThisGame = HWMap(0)#default: invalid map
     if (teamSize == 1):
         mapThisGame = HWMap(random.randint(1,18))
     elif (teamSize == 2):
@@ -125,57 +147,39 @@ while (remainingUnallocatedHumans > 0):                    #this code supports m
         mapThisGame = HWMap(random.randint(15,18))
     print("  MAP: " + str(mapThisGame.name))
     
+    #STEP 4.4: RANDOMIZE WHICH TEAM EACH HUMAN IS ON
     rows, cols = (teamSize*2, 2)                                    #initialize the player array according to team size (and thus # of players in game)
-    arr = [[0 for i in range(cols)] for j in range(rows)]#array, where index = player number, 1st value = [(player ID) if human, 0 if AI], 2nd value = leader ID [1-10] (note that AIS can never have 1 here)
-    for i in range(humansThisGame):                        #assign each player a slot in array. First slots of each team prioritized.
+    arr = [[0 for i in range(cols)] for j in range(rows)]#array, where index = player number, 1st value = [(human ID) if human, 0 if AI], 2nd value = leader ID [1-10] (note that AIS can never have 1 here)
+    for i in range(humansThisGame):                        #assign each human a slot in array.
         desiredSlot = random.randint(0,1)*teamSize
         while (arr[desiredSlot][0] != 0):
             desiredSlot = desiredSlot + 1
             if (desiredSlot >= rows):
                 desiredSlot = 0
-        arr[desiredSlot][0] = (i+1)#player # = (i+1)
+        arr[desiredSlot][0] = randomizedHumanArray[currentHumanIndex]#player # = random human ID (this array is pre-randomized)
+        currentHumanIndex = currentHumanIndex + 1
     
+    #STEP 4.5: RANDOMIZE WHICH LEADER EACH PLAYER WILL BE (AIs are players too)
     for i in range(rows):#RNG each player's leader
-        if (arr[i][0] != 0):#if slot is not AI
+        if (arr[i][0] != 0):#if a human player is in this slot
             arr[i][1] = HWLeader(random.randint(1,10))
-        else:
+        else:#if an AI player is in this slot (AIs cannot play as the United Rebel Front in this game)
             arr[i][1] = HWLeader(random.randint(2,10))
     
-    
-    
-    print("  TEAM ALPHA:")
-    for i in range((int)(rows/2)):#team 1
+    #STEP 4.6: PRINT RESULTS. FIX SPACING TO DYNAMICALLY RESIZE OUTPUT BASED ON NUMBER OF PLAYERS USING str.rjust().
+    for i in range((int)(rows)):
+        if (i == 0):
+            print("  TEAM ALPHA:")
+        elif(i == (int)(rows/2)):
+            print("  TEAM BRAVO:")
         if (arr[i][0] != 0):
-            if (arr[i][0]+lastGamesHighestPlayerNum < 10):
-                print("    Player   " + str(arr[i][0]+lastGamesHighestPlayerNum) + ": " + str(arr[i][1].name))
-            elif(arr[i][0]+lastGamesHighestPlayerNum < 100):
-                print("    Player  " + str(arr[i][0]+lastGamesHighestPlayerNum) + ": " + str(arr[i][1].name))
-            else:
-                print("    Player " + str(arr[i][0]+lastGamesHighestPlayerNum) + ": " + str(arr[i][1].name))
+            print("    Player " + str(arr[i][0]).rjust(len(str(humanCount)),' ') + ": " + str(arr[i][1].name))
         else:
-            print("    AI        " + ": " + str(arr[i][1].name))
-        
-    print("  TEAM BRAVO:")
-    for j in range((int)(rows/2)):#team 2
-        i = j + (int)(rows/2)
-        if (arr[i][0] != 0):
-            if (arr[i][0]+lastGamesHighestPlayerNum < 10):
-                print("    Player   " + str(arr[i][0]+lastGamesHighestPlayerNum) + ": " + str(arr[i][1].name))
-            elif(arr[i][0]+lastGamesHighestPlayerNum < 100):
-                print("    Player  " + str(arr[i][0]+lastGamesHighestPlayerNum) + ": " + str(arr[i][1].name))
-            else:
-                print("    Player " + str(arr[i][0]+lastGamesHighestPlayerNum) + ": " + str(arr[i][1].name))
-        else:
-            print("    AI        " + ": " + str(arr[i][1].name))
-    
+            print("    AI     " + str().rjust(len(str(humanCount)),' ') + ": " + str(arr[i][1].name))
+   
+    #STEP 4.7: FINISH LOOP AND PREPARE VARIABLES FOR NEXT ITERATION.    
     numGamesLeft = numGamesLeft - 1
-    lastGamesHighestPlayerNum = lastGamesHighestPlayerNum + humansThisGame
     
-    
-    
-
-
-
-
+#STEP 5: END PROGRAM ONCE LOOP FINISHES
 print()
 input("Press enter to close...")
